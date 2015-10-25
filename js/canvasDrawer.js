dc876e2035e9da03e5d77cc8b22b050051bf2ae5
Beletskiy <Alexey1Beletskiy@gmail.com>
2015-10-25 06:59:56 +0200

    25_10

--- js/canvasDrawer.js
+++ js/canvasDrawer.js
@@ -1,5 +1,6 @@
 function CanvasDrawer () {
     this.cellSize = 40;
+    this.colors = ['red', 'orange', 'yellow', 'green', 'aqua', 'blue', 'purple', 'silver'];
     this.canvasField = document.getElementById("canvasGameField");
     this.ctx = this.canvasField.getContext('2d');
 }
@@ -10,19 +11,21 @@ CanvasDrawer.prototype.drawField = function (modelArr) {
         ctx = this.ctx,
         width = canvasArr.length,
         height = canvasArr[0].length,
+        colorOfTile;
         offset = 0.5;
 
     this.canvasField.width = this.cellSize*width + offset*2;
     this.canvasField.height = this.cellSize*height + offset*2;
 
-    ctx.strokeStyle = 'black';
-    ctx.lineWidth = 2;
-    ctx.strokeRect(offset, offset, width*cellSize , height*cellSize );
+    /*ctx.strokeStyle = 'black';
+    ctx.lineWidth = 1;
+    ctx.strokeRect(offset, offset, width*cellSize , height*cellSize ); */
 
     for (var j = 0; j < height; j++) {
         for (var i = 0; i < width; i++) {
-
+            colorOfTile = this.colors[canvasArr[i][j][0]];
+            this.ctx.fillStyle = colorOfTile;
+            this.ctx.fillRect(i*this.cellSize, j*this.cellSize, this.cellSize, this.cellSize);
         }
     }
-    ctx.stroke();
 };
